8c35c0af6b7f024b6467057f0a7c7155d8310d1a
Hugo Minzoni <hugominzoni@gmail.com>
2023-02-09 15:08:42 -0500

    add portfolio popup in production

--- app.js
+++ app.js
@@ -26,3 +26,46 @@ function search(){
     paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
     console.log(paragraph)
 }
+
+
+// PORTFOLIO 
+
+const imageThumb = document.querySelectorAll('.open-port')
+const imageBox = document.querySelector('.port-box')
+const closeBox = document.querySelector('.close-port')
+const overlay = document.querySelector('.over')
+const imgBig = document.querySelectorAll('.imgBig')
+
+for(let i = 0; i < imgBig.length; i++){
+    imgBig[i].addEventListener('click', function(){})
+}
+
+
+
+
+
+
+for(let i = 0; i < imageThumb.length;i++){
+    imageThumb[i].addEventListener('click', function(){
+        imgBig[i].classList.remove('hidden')
+        imageBox.classList.remove('hidden')
+        closeBox.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+
+
+closeBox.addEventListener('click', function(){
+    imgBig[i].classList.add('hidden')
+    imageBox.classList.add('hidden')
+    overlay.classList.add('hidden')
+    closeBox.classList.add('hidden')
+})
+
+overlay.addEventListener('click', function(){
+    imgBig[i].classList.remove('hidden')
+    imageBox.classList.add('hidden')
+    overlay.classList.add('hidden')
+    closeBox.classList.add('hidden')
+})
+
+}
